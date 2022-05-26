5fad929cb49d965135cc92876980dad2a6495794
Vincent <vincopi@googlemail.com>
2022-05-26 19:36:16 +0200

    adjust script for second topic

--- connect/delete-config.sh
+++ connect/delete-config.sh
@@ -2,8 +2,13 @@
 
 KAFKA_CONNECT_ADDRESS=${1:-localhost}
 KAFKA_CONNECT_PORT=${2:-8083}
-BASE_CONFIG=${3:-"$(dirname $0)/elastic-sink.json"}
+LOBBY_CONFIG=${3:-"$(dirname $0)/elastic_sink_corporate.json"}
+CORPORATE_CONFIG=${3:-"$(dirname $0)/elastic_sink_lobby.json"}
+
 KAFKA_CONNECT_API="$KAFKA_CONNECT_ADDRESS:$KAFKA_CONNECT_PORT/connectors"
 
-CONNECTOR_NAME=$(jq -r .name $BASE_CONFIG)
-curl -Is -X DELETE $KAFKA_CONNECT_API/$CONNECTOR_NAME
+LOBBY_NAME=$(jq -r .name $LOBBY_CONFIG)
+CORPORATE_NAME=$(jq -r .name $CORPORATE_CONFIG)
+
+curl -Is -X DELETE $KAFKA_CONNECT_API/$LOBBY_NAME
+curl -Is -X DELETE $KAFKA_CONNECT_API/$CORPORATE_NAME

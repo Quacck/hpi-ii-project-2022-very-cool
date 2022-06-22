bf2319fc37966a380998e0cc2514194388692037
Vincent <vincopi@googlemail.com>
2022-06-22 13:03:02 +0200

    Query person-events via request
    Co-authored-by: Mark Bader <markbader@users.noreply.github.com>

--- duplicate_detection/main.py
+++ duplicate_detection/main.py
@@ -1,5 +1,18 @@
+import requests
+from sympy import Q
+
 def main():
-    pass
+    url ="http://localhost:9200/person-events/_search?pretty"
+    query = """ 
+    { \"query\": { 
+            \"match_all\": { }
+        }
+    }
+    """
+    request = requests.get(url, headers={"Content-Type": "application/json"}, data=query)
+    print(request.content)
+
+    
 
 if __name__=="__main__":
     main()

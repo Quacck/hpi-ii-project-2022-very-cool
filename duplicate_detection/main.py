f14383f0aab62585432186150721c42bc60bf613
Vincent <vincopi@googlemail.com>
2022-06-29 15:36:26 +0200

    Try to calculate transitive closure

--- duplicate_detection/main.py
+++ duplicate_detection/main.py
@@ -1,28 +1,65 @@
+import uuid
 from elasticsearch import Elasticsearch, helpers
+import editdistance
+from torch import threshold, threshold_
 
 es = Elasticsearch('http://localhost:9200')
 
 
 def main():
-        window_size = 10
+        window_size = 5
         generator = helpers.scan(client=es, query= {"sort": ["city"]}, scroll="5m", index="person-events", preserve_order=True)
         window = [next(generator)]
 
+        duplicates: set[tuple[str]] = set()
+
         # LOOP:
-        while len(window) > 0:
-            window.append(next(generator))
-            print(window[-1]["_source"]["city"])
-            if len(window) >= window_size: # fill up the window in the beginning
-                window.pop(0)
+        try:
+            while len(window) > 0:
+                window.append(next(generator))
+                if len(window) >= window_size: # fill up the window in the beginning
+                    window.pop(0)
 
-            for to_be_compared_element in window[:-1]:
+                for to_be_compared_element in window[:-1]:
 
-                if check_for_duplicate(to_be_compared_element, window[-1]):
-                    print('oh snap!')
+                    if similarity(to_be_compared_element, window[-1]) > 0.8:
+                        first_id = to_be_compared_element["_source"]["id"]
+                        second_id = window[-1]["_source"]["id"]
+                        # print(f'{first_id} is similar to {second_id}')
+                        duplicates.add((first_id, second_id))
+                        duplicates.add((second_id, first_id)) # make symmetric
 
-def check_for_duplicate(a, b):
-    # print(f'{a["_source"]["city"]} gets compared to {b["_source"]["city"]}')
-    return a["_source"]["first_name"] == b["_source"]["first_name"]
+        except StopIteration:
+            # yoink
+            print(f'FOUND {len(duplicates)} DUPLICATES')
+            transitive_duplicates: set[tuple[str]] = transitive_closure(duplicates)
+            for duplicate in transitive_duplicates:
+                es.create(index="person_duplicates", id=uuid.uuid4(), body={duplicate[0], duplicate[1]})
+
+
+def similarity(a, b):
+    # 1 is most similar, 0 is not similar
+
+    distance_first_names = 1 / max(editdistance.eval(a["_source"]["first_name"], b["_source"]["first_name"]), 1)
+    distance_last_names = 1 / max(editdistance.eval(a["_source"]["last_name"], b["_source"]["last_name"]),1 )
+    distance_city = 1 / max(editdistance.eval(a["_source"]["city"], b["_source"]["city"]), 1)
+    distance_birthday = 1 if editdistance.eval(a["_source"]["birth_date"], b["_source"]["birth_date"]) < 2 else 0
+
+    return 0.35 * distance_first_names + 0.25 * distance_last_names + 0.05 * distance_city + 0.35 * distance_birthday
+
+# heavily inspired by https://stackoverflow.com/a/8674062
+def transitive_closure(closure):
+    while True:
+        new_relations = set((x,w) for x,y in closure for q,w in closure if q == y)
+
+        closure_until_now = closure | new_relations
+
+        if closure_until_now == closure:
+            break
+
+        closure = closure_until_now
+
+    return closure
 
 if __name__=="__main__":
     main()
